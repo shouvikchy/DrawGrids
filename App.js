b0fe4be255a80f243413291b04351e631540961d
Shouvik Chowdhury <shouvikchowdhury15@gmail.com>
2023-09-21 11:47:36 +0600

    project initialization

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+
+const App=()=>{
+
+
+  return (
+<View style={{flex:1,backgroundColor:'#FFFFFF'}}>
+
+</View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
